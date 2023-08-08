8ee4177afd10f87445ad62e0f89e31a17eab6329
Michel Diab <70917771+micheldiab@users.noreply.github.com>
2023-08-08 09:39:32 +0300

    Spring boot code

---
+++ Backend/src/test/java/com/michel/friendsapi/FriendsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michel.friendsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
