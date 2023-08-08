e0bdfeb71e4c59b8126584c3c27045768ef1892c
Michel Diab <70917771+micheldiab@users.noreply.github.com>
2023-08-08 09:38:30 +0300

    Delete Backend/friendsapi directory

--- Backend/friendsapi/src/main/java/com/michel/friendsapi/FriendRepository.java
+++
@@ -1,8 +0,0 @@
-package com.michel.friendsapi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FriendRepository extends JpaRepository<Friend, Integer> {
-}
